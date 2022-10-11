2800136d3e278824638dadd1705ecfeb67627e50
Steven Carter <steven.carter@bounteous.com>
2022-10-11 00:26:14 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Flex, Box } from 'reflexbox';
+import {TimeTrackingReport} from './TimeTrackingReport';
 
 function App() {
+    const [source, setSource] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex>
+        <Box>
+            <textarea rows={30} cols={100} value={source} onChange={e => setSource(e.target.value)}></textarea>
+        </Box>
+        <Box p={20}>
+        <TimeTrackingReport source={source} />
+        </Box>
+    </Flex>
   );
 }
 
