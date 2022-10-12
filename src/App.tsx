1c1f6301e7566bac6b6fe3c51085fa0c516831d3
Steven Carter <steven.carter@bounteous.com>
2022-10-11 21:26:36 -0400

    Adding eslint/prettier and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Flex, Box } from 'reflexbox';
-import {TimeTrackingReport} from '@stevenwcarter/time-tracking-components';
+import { TimeTrackingReport } from '@stevenwcarter/time-tracking-components';
 
 function App() {
-    const [source, setSource] = useState('');
+  const [source, setSource] = useState('');
+
   return (
     <Flex>
-        <Box>
-            <textarea rows={30} cols={100} value={source} onChange={e => setSource(e.target.value)}></textarea>
-        </Box>
-        <Box p={20}>
+      <Box>
+        <textarea
+          rows={30}
+          cols={100}
+          value={source}
+          onChange={(e) => setSource(e.target.value)}
+        ></textarea>
+      </Box>
+      <Box p={20}>
         <TimeTrackingReport source={source} />
-        </Box>
+      </Box>
     </Flex>
   );
 }
